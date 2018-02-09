11d29f68b3d385a54e732740ed7cea98c6f1a772
Rexomnis <dpshunter@gmail.com>
2018-02-09 07:29:28 -0600

    Filled out the backbone of the web server
    Signed-off-by: Rexomnis <dpshunter@gmail.com>

---
+++ src/components/triple_search.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TripleSearch extends Component{
+    render(){
+        return (
+            <div>
+                Hi
+            </div>
+        );
+    }
+}
+
+export default TripleSearch;
---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import promise from 'redux-promise';
+
+import reducers from './reducers';
+import TripleSearch from './components/triple_search';
+
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <BrowserRouter>
+            <div>
+                <Switch>
+                    <Route path="/" component={TripleSearch} />
+                </Switch>
+            </div>
+        </BrowserRouter>
+    </Provider>,
+    document.querySelector('.container')
+);
